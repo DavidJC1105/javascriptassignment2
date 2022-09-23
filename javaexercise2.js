30ab1836a2bc599927e67494ca2ea77ab4ae6fb5
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2022-09-23 12:34:38 +0100

    Add files via upload

---
+++ javaexercise2.js
@@ -0,0 +1,23 @@
+let number= Number(prompt("enter a number 1-10"));
+
+console.log(number);
+
+if (number %2==0){
+    console.log("This is an even number")
+} else{
+    console.log("This number is odd")
+}
+
+let higher= Number(prompt("Enter a number"));
+
+console.log(higher);
+
+let lower= Number(prompt("Enter another number"));
+
+if (higher>lower){
+    console.log(Number 1 is higher than number 2)
+} else if(higher<lower){
+    console.log("Number 2 is higher than number 1")
+} else{
+    console.log("The numbers entered are equal");
+}
