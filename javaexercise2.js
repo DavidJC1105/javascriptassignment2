f4066999305c1f392eb1c6e80b807c4ceab53daa
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2022-09-28 10:56:27 +0100

    Add files via upload

--- javaexercise2.js
+++ javaexercise2.js
@@ -15,9 +15,36 @@ console.log(higher);
 let lower= Number(prompt("Enter another number"));
 
 if (higher>lower){
-    console.log(Number 1 is higher than number 2)
+    console.log("Number 1 is higher than number 2")
 } else if(higher<lower){
     console.log("Number 2 is higher than number 1")
 } else{
     console.log("The numbers entered are equal");
 }
+
+let number1= Number(prompt("Enter your first number"))
+console.log(number1)
+let number2= Number(prompt("Enter your second number"))
+console.log(number2)
+let number3= Number(prompt("Enter your third number"))
+console.log(number3)
+
+if(number1==number2){
+    if(number2==number3){
+        console.log("All numbers are equal")
+    }
+}
+if(number1>number2){
+    if(number1>number3){
+        console.log("Number 1 is the highest number")
+    }else{if(number3>number1){
+        console.log("number 3 is the highest")
+    }
+    }
+}
+else{
+    if(number2>number3){
+        console.log("Number 2 is the highest number")
+    }else{console.log("Number 3 is the highest")}
+}
+
